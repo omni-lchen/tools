776bc5fad07de81e5a61462754bb2ba56681645c
omni-lchen <lchen@omnifone.com>
2015-10-06 16:47:25 +0100

    Initial commit for testing

---
+++ src/main/java/com/lchen/tools/JVMTool.java
@@ -0,0 +1,30 @@
+package com.lchen.tools;
+
+import java.lang.management.ManagementFactory;
+import java.util.Iterator;
+import java.util.Set;
+import javax.management.MBeanServer;
+import javax.management.ObjectInstance;
+
+/**
+ * Created by lchen on 06/10/15.
+ */
+public class JVMTool {
+
+    JVMTool(){
+
+    }
+
+    void listAllJVMMBeans() {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        Set<ObjectInstance> instances = server.queryMBeans(null, null);
+        Iterator<ObjectInstance> iterator = instances.iterator();
+        while (iterator.hasNext()) {
+            ObjectInstance instance = iterator.next();
+            System.out.println("MBean Found:");
+            System.out.println("Class Name:t" + instance.getClassName());
+            System.out.println("Object Name:t" + instance.getObjectName());
+            System.out.println("****************************************");
+        }
+    }
+}
---
+++ src/main/java/com/lchen/tools/Main.java
@@ -0,0 +1,11 @@
+package com.lchen.tools;
+
+/**
+ * Created by lchen on 06/10/15.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JVMTool lmb = new JVMTool();
+        lmb.listAllJVMMBeans();
+    }
+}
